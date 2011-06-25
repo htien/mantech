ad76b13ff685a6c28ce23663f060a942dd78adc5
Tien Nguyen <lilylnx@gmail.com>
2011-06-25 08:04:08 +0700

    Đã FIXED xong lỗi Eclipse không nhận thư mục 'WEB-INF/lib'. Cập nhật HibernateGenericDAO và license.txt

---
+++ net.lilylnx.springnet/src/main/java/net/lilylnx/springnet/core/hibernate/HibernateGenericDAO.java
@@ -0,0 +1,16 @@
+/**
+ * Written by Tien Nguyen <lilylnx@users.sf.net>
+ * FREE FOR ALL BUT DOES NOT MEAN THERE IS NO PRICE.
+ */
+package net.lilylnx.springnet.core.hibernate;
+
+import net.lilylnx.springnet.repository.Repository;
+
+
+/**
+ * @author Tien Nguyen
+ * @version $Id: HibernateGenericDAO.java,v 1.0 2011/06/24 1:54:03 lilylnx Exp $
+ */
+public abstract class HibernateGenericDAO<T> implements Repository<T> {
+
+}

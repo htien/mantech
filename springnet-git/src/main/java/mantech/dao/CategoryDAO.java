fb4f19131d8e222f40563de7b23f631c75c68d64
Long Nguyen <chautinhlong@gmail.com>
2011-09-06 01:58:48 +0700

    Create DAO and Repository for Category.

---
+++ springnet-git/src/main/java/mantech/dao/CategoryDAO.java
@@ -0,0 +1,25 @@
+/**
+ * Written by Tien Nguyen <lilylnx@users.sf.net>
+ * FREE FOR ALL BUT DOES NOT MEAN THERE IS NO PRICE.
+ */
+package mantech.dao;
+
+import org.hibernate.SessionFactory;
+
+import mantech.domain.Category;
+import mantech.repository.CategoryRepository;
+
+import net.lilylnx.springnet.core.hibernate.HibernateGenericDAO;
+
+/**
+ * @author Long Nguyen
+ * @version $Id: CategoryDAO.java,v 1.0 2011/09/06 1:57:05 longnguyen Exp $
+ */
+public class CategoryDAO extends HibernateGenericDAO<Category> implements
+		CategoryRepository {
+
+	public CategoryDAO(SessionFactory sessionFactory) {
+		super(sessionFactory);
+	}
+
+}
---
+++ springnet-git/src/main/java/mantech/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+/**
+ * Written by Tien Nguyen <lilylnx@users.sf.net>
+ * FREE FOR ALL BUT DOES NOT MEAN THERE IS NO PRICE.
+ */
+package mantech.repository;
+
+import mantech.domain.Category;
+
+import net.lilylnx.springnet.repository.Repository;
+
+/**
+ * @author Long Nguyen
+ * @version $Id: CategoryRepository.java,v 1.0 2011/09/06 1:56:06 longnguyen Exp $
+ */
+public interface CategoryRepository extends Repository<Category> {
+
+}

956ddfe45c77c2f6e8573c7c0c72c722e0e3cc70
Tien Nguyen <lilylnx@gmail.com>
2011-10-03 00:25:29 +0700

    Renamed some of method in Repository. And created saveOrUpdateAll() method.

--- springnet-git/src/main/java/net/lilylnx/springnet/repository/Repository.java
+++ springnet-git/src/main/java/net/lilylnx/springnet/repository/Repository.java
@@ -5,6 +5,7 @@
 package net.lilylnx.springnet.repository;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -26,19 +27,25 @@ public interface Repository<T> {
    * Thêm một thực thể mới của một đối tượng vào repository.
    * @param entity Thực cần lưu
    */
-  void add(T entity);
-
-  /**
-   * Xóa một thực thể từ repository.
-   * @param entity Thực thể cần xóa
-   */
-  void remove(T entity);
+  Serializable save(T entity);
 
   /**
    * Cập nhật thông tin của một thực thể trong repository.
    * @param entity Thực thể cần cập nhật
    */
   void update(T entity);
+
+  /**
+   * Xóa một thực thể từ repository.
+   * @param entity Thực thể cần xóa
+   */
+  void delete(T entity);
+  
+  /**
+   * Cập nhật một danh sách các thực thể trong repository.
+   * @param entities Tập hợp/danh sách cùng một thực thể
+   */
+  void saveOrUpdateAll(final Collection<T> entities);
   
   // Bổ sung thêm đồ chơi :D
   
